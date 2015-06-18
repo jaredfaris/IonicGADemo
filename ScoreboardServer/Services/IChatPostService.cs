a283c347e84c7e56ca4889fa14d5b0fab75c2a2b
Jared Faris <jfaris@gmail.com>
2015-06-18 14:21:12 -0400

    Added fake slack poster class

---
+++ ScoreboardServer/Services/IChatPostService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreboardServer.Services
+{
+    interface IChatPostService
+    {
+        void PostToSlack(string textToPost);
+    }
+
+    class FakeChatPostService : IChatPostService
+    {
+        public void PostToSlack(string textToPost)
+        {
+            return;
+        }
+    }
+}
